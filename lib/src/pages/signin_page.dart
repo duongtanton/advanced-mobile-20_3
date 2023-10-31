27b3af475e5f48ddc6f30699ebbc8fd03e700333
20120598 <20120598@student.hcmus.edu.vn>
2023-10-31 20:59:18 +0700

    Add full a sign-in page.

--- lib/src/pages/signin_page.dart
+++ lib/src/pages/signin_page.dart
@@ -11,6 +11,20 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  bool _obscured = false;
+
+  final textFieldFocusNode = FocusNode();
+
+  void _toggleObscured() {
+    setState(() {
+      _obscured = !_obscured;
+      if (textFieldFocusNode.hasPrimaryFocus) {
+        return;
+      }
+      textFieldFocusNode.canRequestFocus = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,23 +36,101 @@ class _SignInPageState extends State<SignInPage> {
             child: Column(
               children: [
                 const Image(
-                    image: AssetImage("assets/images/sign-in-banner.png")),
+                    image: AssetImage("assets/images/sign-in-banner.png"),
+                    height: 250),
                 Container(
                   padding: const EdgeInsets.only(left: 10, right: 10),
                   child: const Column(
                     children: [
+                      Padding(padding: EdgeInsets.only(top: 40)),
                       Text("Đăng nhập",
                           style: TextStyle(
-                              fontSize: 44,
+                              fontSize: 34,
                               color: Color.fromRGBO(0, 113, 240, 1))),
+                      Padding(padding: EdgeInsets.only(top: 20)),
                       Text(
-                        "Phát triển kỹ năng tiếng Anh nhanh nhất bằng cách học 1 " +
-                            "kèm 1 trực tuyến theo mục tiêu và lộ trình dành cho riêng bạn.",
+                        "Phát triển kỹ năng tiếng Anh nhanh nhất bằng cách học 1 "
+                        "kèm 1 trực tuyến theo mục tiêu và lộ trình dành cho riêng bạn.",
                         textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 18,
                         ),
                       )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 40),
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: "mail@example.com",
+                            label: Text("ĐỊA CHỈ EMAIL")),
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 20)),
+                      TextField(
+                        obscureText: _obscured,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: InputDecoration(
+                            label: const Text("MẬT KHẨU"),
+                            suffixIcon: GestureDetector(
+                                onTap: _toggleObscured,
+                                child: Icon(
+                                  _obscured
+                                      ? Icons.visibility_rounded
+                                      : Icons.visibility_off_rounded,
+                                  size: 24,
+                                ))),
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 50)),
+                      const Text("Quên mật khẩu?", textAlign: TextAlign.start),
+                      const Padding(padding: EdgeInsets.only(top: 10)),
+                      TextButton(
+                          onPressed: () => {Navigator.pushNamed(context, '/')},
+                          style: ButtonStyle(
+                              padding:
+                                  const MaterialStatePropertyAll<EdgeInsets>(
+                                      EdgeInsets.only(left: 30, right: 30)),
+                              backgroundColor: MaterialStateColor.resolveWith(
+                                  (states) =>
+                                      const Color.fromRGBO(0, 113, 240, 1))),
+                          child: const Text("ĐĂNG NHẬP",
+                              style: TextStyle(color: Colors.white))),
+                      const Padding(padding: EdgeInsets.only(top: 30)),
+                      const Text("Hoặc tiếp tục với"),
+                      Container(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset("assets/images/fb.svg",
+                                  height: 40),
+                              const Padding(
+                                  padding: EdgeInsets.only(left: 6, right: 6)),
+                              SvgPicture.asset("assets/images/gg.svg",
+                                  height: 40),
+                              const Padding(
+                                  padding: EdgeInsets.only(left: 6, right: 6)),
+                              SvgPicture.asset("assets/images/mb.svg",
+                                  height: 40)
+                            ],
+                          )),
+                      const Padding(padding: EdgeInsets.only(top: 20)),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Bạn chưa có tài khoản? "),
+                          Text(
+                            "Đăng ký",
+                            style: TextStyle(
+                                color: Colors.blue,
+                                decoration: TextDecoration.underline),
+                          ),
+                        ],
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 80)),
                     ],
                   ),
                 )
